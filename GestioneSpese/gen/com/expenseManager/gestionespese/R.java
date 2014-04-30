a388456648dc46c2a502c2d6d82b88c182c28511
riccardo <r.barba2@studenti.unisa.it>
2014-04-30 21:33:21 +0200

    Aggiornamento grafica
    Signed-off-by: riccardo <r.barba2@studenti.unisa.it>

--- GestioneSpese/gen/com/expenseManager/gestionespese/R.java
+++ GestioneSpese/gen/com/expenseManager/gestionespese/R.java
@@ -10,6 +10,10 @@ package com.expenseManager.gestionespese;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f060000;
+        public static final int white=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,18 +28,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090001;
+        public static final int date=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int data=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int myApp=0x7f050003;
     }
     public static final class style {
         /** 
@@ -59,10 +66,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        /**   My Theme  
+         */
+        public static final int MyTheme=0x7f070002;
     }
 }
--- GestioneSpese/src/com/expenseManager/gestionespese/MainActivity.java
+++ GestioneSpese/src/com/expenseManager/gestionespese/MainActivity.java
@@ -1,8 +1,11 @@
 package com.expenseManager.gestionespese;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,25 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//Setta la data
+		Calendar data=Calendar.getInstance();
+		TextView var=(TextView) this.findViewById(R.id.date);
+		switch(data.get(Calendar.MONTH))
+		{
+		case 0: var.setText("Gennaio,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 1: var.setText("Febbraio,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 2: var.setText("Marzo,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 3: var.setText("Aprile,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 4: var.setText("Maggio,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 5: var.setText("Giugno,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 6: var.setText("Luglio,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 7: var.setText("Agosto,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 8: var.setText("Settembre,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 9: var.setText("Ottobre,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 10: var.setText("Novembre,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		case 11: var.setText("Dicembre,"+data.get(Calendar.DAY_OF_MONTH)+","+data.get(Calendar.YEAR));break;
+		}
+		
 	}
 
 	@Override
