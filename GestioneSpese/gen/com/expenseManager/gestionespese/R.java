49086c7248ded1009ee96bc292e45d6ce0663584
riccardo <r.barba2@studenti.unisa.it>
2014-05-03 16:30:01 +0200

    Grafica activity principale
    Signed-off-by: riccardo <r.barba2@studenti.unisa.it>

--- GestioneSpese/gen/com/expenseManager/gestionespese/R.java
+++ GestioneSpese/gen/com/expenseManager/gestionespese/R.java
@@ -11,7 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int altro=0x7f060004;
         public static final int blue=0x7f060000;
+        public static final int conto=0x7f060003;
+        public static final int entrata=0x7f060005;
+        public static final int impostazioni=0x7f060008;
+        public static final int panoramica=0x7f060006;
+        public static final int report=0x7f060007;
+        public static final int spesa=0x7f060002;
         public static final int white=0x7f060001;
     }
     public static final class dimen {
@@ -25,11 +32,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gestionespese=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int date=0x7f090000;
+        public static final int action_settings=0x7f09000b;
+        public static final int altro=0x7f090005;
+        public static final int conto=0x7f090004;
+        public static final int date=0x7f090002;
+        public static final int entrata=0x7f090009;
+        public static final int griglia_dati=0x7f090003;
+        public static final int impostazioni=0x7f090008;
+        public static final int layout_principale=0x7f090000;
+        public static final int name_app=0x7f090001;
+        public static final int panoramica=0x7f090006;
+        public static final int report=0x7f090007;
+        public static final int spesa=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,11 +55,19 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
+        public static final int Report1=0x7f05000c;
         public static final int action_settings=0x7f050001;
+        public static final int altro=0x7f05000b;
         public static final int app_name=0x7f050000;
+        public static final int conto=0x7f050006;
         public static final int data=0x7f050004;
+        public static final int entrata=0x7f050008;
         public static final int hello_world=0x7f050002;
+        public static final int impostazioni=0x7f050007;
         public static final int myApp=0x7f050003;
+        public static final int panoramica=0x7f050009;
+        public static final int report=0x7f05000a;
+        public static final int spesa=0x7f050005;
     }
     public static final class style {
         /** 
